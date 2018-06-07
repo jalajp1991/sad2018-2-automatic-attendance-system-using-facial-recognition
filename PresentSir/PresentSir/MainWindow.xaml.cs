a148ca060ffa599aadac6b2a4fbb1e0be943c6d8
Pankaj Singh <resopankaj@gmail.com>
2018-06-07 17:43:26 +0200

    Add files via upload
    first commit - added project structure and basic user interface for the application.

---
+++ PresentSir/PresentSir/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace PresentSir
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private void BTn_Detect_Click(object sender, RoutedEventArgs e)
+        {
+      
+        }
+
+        private void BTn_ResetAttendance_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BTn_ViewAttendance_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+        
+        private void BTn_AddStudent_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BTn_StartCamera_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void BTn_ExtractFeatures_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        
+      
+    }
+}
